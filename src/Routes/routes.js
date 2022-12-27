6cd433ee842c23328417f1b4417f5e3672fbcb5f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-27 14:06:48 +0600

    nav footer home create

---
+++ src/Routes/routes.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
