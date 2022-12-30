f9bd3e500f794dd0b420479d65bcfdf5c7273ff8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-30 16:12:30 +0600

    error solved

--- src/Pages/Home/AllMedia/SignlePost.js
+++ src/Pages/Home/AllMedia/SignlePost.js
@@ -167,20 +167,21 @@ const SignlePost = ({ post }) => {
                 </svg>
               </button>
             </Link>
-
-            <button
-              type="button"
-              title="Share post"
-              className="flex items-center justify-center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className="w-7 h-6 fill-current"
+            <Link to={`/media/${_id}`}>
+              <button
+                type="button"
+                title="Share post"
+                className="flex items-center justify-center"
               >
-                <path d="M474.444,19.857a20.336,20.336,0,0,0-21.592-2.781L33.737,213.8v38.066l176.037,70.414L322.69,496h38.074l120.3-455.4A20.342,20.342,0,0,0,474.444,19.857ZM337.257,459.693,240.2,310.37,389.553,146.788l-23.631-21.576L215.4,290.069,70.257,232.012,443.7,56.72Z"></path>
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="w-7 h-6 fill-current"
+                >
+                  <path d="M474.444,19.857a20.336,20.336,0,0,0-21.592-2.781L33.737,213.8v38.066l176.037,70.414L322.69,496h38.074l120.3-455.4A20.342,20.342,0,0,0,474.444,19.857ZM337.257,459.693,240.2,310.37,389.553,146.788l-23.631-21.576L215.4,290.069,70.257,232.012,443.7,56.72Z"></path>
+                </svg>
+              </button>
+            </Link>
           </div>
           <div className="flex justify-between pt-3 pb-1">
             <div className="flex items-center space-x-2">
@@ -188,7 +189,7 @@ const SignlePost = ({ post }) => {
                 <div className="text-sm">
                   Liked by
                   <span className="font-semibold pl-1">
-                    {totallike < 1000 ? totallike : totallike / 1000 + "k"}
+                    {totallike < 1000 ? totallike : totallike / 1000 + " k"}
                     <span className="pl-1"> others</span>
                   </span>
                 </div>
